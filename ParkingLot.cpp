75cf6d2574229c62baa45a755d98debb6e7a76c0
Anirudh Bansal <anirudhbansal005@gmail.com>
2026-01-07 19:05:26 +0530

    add car and bikes via inheritance and a base vehicle class

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -3,28 +3,45 @@
 using namespace std;
 
 class ParkingLot;
-class Car {
+class vehicle {
     string licensePlate;
     string ownerName;
-    friend void debug(ParkingLot p);
-    friend class ParkingLot;
-    public:
-        Car(){
-        };
-        Car(string name, string plate){
-            ownerName = name;
+    int entryTime;
+    public: 
+        friend class ParkingLot;
+        friend void debug(ParkingLot p);
+        vehicle(){};
+        vehicle(string plate, string name, int time){
             licensePlate = plate;
+            ownerName = name;
+            entryTime = time;
         }
+
+        string getPlate(){
+            return licensePlate;
+        }
+        virtual ~vehicle() {};
+};
+
+class bike : public vehicle {
+    public:
+        bike(){};
+        bike(string plate, string name, int time) : vehicle(plate, name, time) {}
+};
+class Car : public vehicle {
+    public:
+        Car(){};
+        Car (string plate, string name, int time) : vehicle(plate, name, time) {};
 };
 
 class ParkingLot{
     private:
-        vector<Car>spots;
+        vector<vehicle*>spots;
         int capacity;
         int curr;
-        public:
+    public:
         ParkingLot(int cap){
-            spots.resize(cap);
+            spots.resize(cap, nullptr);
             capacity = cap;
             curr = 0;
         }
@@ -35,14 +52,14 @@ class ParkingLot{
         bool IsEmpty(){
             return curr==0;
         }
-        void park(Car c){
+        void park(vehicle* v){
             if(IsFull()){
                 cout<<"Full! couldnt park"<<endl;
                 return;
             }
             for(int i=0; i<capacity; i++){
-                if(spots[i].licensePlate==""){
-                    spots[i] = c;
+                if(spots[i]==nullptr){
+                    spots[i] = v;
                     curr++;
                     cout<<"Parked"<<endl;
                     break;
@@ -50,48 +67,61 @@ class ParkingLot{
             }
             
         }
-        void leave(Car c){
-            if(IsEmpty()){
-                return;
-            }
-            for(int i=0; i<capacity; i++){
-                if(spots[i].licensePlate==c.licensePlate){
-                    spots[i].licensePlate="";
-                    curr--;
-                    cout<<"Left "<<c.ownerName<<endl;
-                    break;
-                }
+       void leave(string plate) {
+        if(IsEmpty()) return;
+        
+        for(int i=0; i<capacity; i++){
+            if(spots[i] != nullptr && spots[i]->getPlate() == plate){
+                cout << "Left " << spots[i]->getPlate() << endl;
+                spots[i] = nullptr;
+                curr--;
+                break;
             }
         }
+    }
 };
 
 void debug(ParkingLot p) {
     cout << "\n--- [DEBUG VIEW] ---" << endl;
     for(int i=0; i<p.capacity; i++) {
-        if(p.spots[i].licensePlate == "") 
+        if(p.spots[i] == nullptr) 
             cout << "Slot " << i << ": [ Empty ]" << endl;
         else 
-            cout << "Slot " << i << ": [ " << p.spots[i].licensePlate << " ]" << endl;
+            cout << "Slot " << i << ": [ " << p.spots[i]->licensePlate << " ]" << endl;
     }
     cout << "--------------------\n" << endl;
 }
+
+void parkTool(ParkingLot* pl){
+    string m, p;
+    int t;
+    bool isCar = true;
+    cin >> isCar;
+    cin >> m >> p;
+    cin >> t;
+    if(isCar){
+        Car* v = new Car(p, m, t);    
+        pl->park(v);
+        debug(*pl);
+    }
+    else {
+        bike* v = new bike(p, m, t);
+        pl->park(v);
+        debug(*pl);
+        pl->leave(p);
+        debug(*pl);
+    }
+}
+
 int main(){
-    string n, p;
-    cin >> n >> p;
-    Car c(n, p);
-    ParkingLot pl(5);
-    pl.park(c);
-    debug(pl);
-    cin >> n >> p;
-    Car c2(n, p);
-    pl.leave(c);
-    debug(pl);
-    pl.park(c2);
-    debug(pl);
-    cin >> n >> p;
-    Car c3(n, p);
-    pl.park(c3);
-    debug(pl);
-    pl.leave(c2);
-    debug(pl);
+    cout<<"Enter Number of vehicles"<<endl;
+    int n;
+    cin >> n;
+    ParkingLot* pl = new ParkingLot(n);
+    debug(*pl);
+    for(int i=0; i<n; i++){
+        cout<<"Bool Name Plate Time"<<endl;
+        parkTool(pl);
+    }
+    
 }
